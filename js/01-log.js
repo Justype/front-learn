b01a60dad2777957e60f42b83584c50518f7ca15
justype <3424005241@qq.com>
2019-09-22 19:59:44 +0800

    complete log

--- js/01-log.js
+++ js/01-log.js
@@ -1,1 +1,13 @@
-console.log(`Hello World`)
+/**
+ * 最基本的输入，输出
+ * 
+ * 通过开发者工具控制台的形式来输出
+ * 
+ * 注意字符串有 '' 或 "" 或 ``
+ * `${变量}` 的中间可以插入变量
+ */
+
+console.log(`console log`);     // 普通输出
+console.warn("console warn");   // 警告输出
+console.error('console error'); // 错误输出
+
