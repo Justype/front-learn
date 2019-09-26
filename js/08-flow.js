3fc060cf18c8cf7d3e6a30c51f50ac2009592dd8
justype <3424005241@qq.com>
2019-09-26 11:37:40 +0800

    flow

--- js/08-flow.js
+++ js/08-flow.js
@@ -1,17 +1,30 @@
-/**
- * JS的流程控制语句与其他的语言相似
- * 
- * 分支语句
- * 
- * 循环
- * 
- */
+/*
+JS的流程控制语句与其他的语言相似
+
+分支语句
+    if-else if-else
+        与其他语言相似
+    switch(){case xxx: break; default: break;}
+        case判断的是===
+        break; 不是必须，不添加，继续向下执行
+        default: 可以放到任意位置，不是必须
+循环
+    while(){}
+    do{}while()
+    for(;;){}
+        都是类似的
+        break; continue;
+*/
 
 // 分支语句 if-else if-else
 // if
-if(true){
-    console.log(true);
+let x = 5;
+if(x<0){
+    console.log('x<0');
+}else if(x>0){
+    console.log('x>0');
 }else{
-    console.log(false);
-}   // true
+    console.log('x=0');
+}
+
 
